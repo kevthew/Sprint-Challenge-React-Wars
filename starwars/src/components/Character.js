ef4218b95acc2b2da667403a6e0e53b1d261bf60
kevin thew <kevthew@outlook.com>
2020-07-31 20:54:57 -0600

    redo

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,32 +1,53 @@
 // Write your Character component here
-import React from 'react';
-import {Card, CardTitle, CardSubtitle, CardBody, Container, Row, Col} from '../../node_modules/reactstrap'
+import React from "react";
+import {
+  Card,
+  CardTitle,
+  CardSubtitle,
+  CardBody,
+  Container,
+  Row,
+  Col,
+} from "../../node_modules/reactstrap";
+import styled from "styled-components";
 
-function Character(props){
-    return(
-        
-        <Container>
-            <Row>          
-                    {props.characterArray.map((character)=>{
-                        return(
-                            <Col xs='4'>
-                                <Card key={character.created}>
-                                        <CardTitle>{character.name}</CardTitle>
-                                        <CardSubtitle>Gender: {character.gender}</CardSubtitle>
-                                        <CardBody>
-                                            Born: {character.birth_year}<br/>
-                                            Height: {character.height}<br/>
-                                            Mass: {character.mass}
-                                        </CardBody>
-                                </Card> 
-                            </Col>
-                        )
-                    })}
-            </Row>
-        </Container>
+const BackgroundDiv = styled.div`
+  background: tomato;
+`;
 
-        
-    )
+const ContentTxt = styled.p`
+  color: grey;
+`;
+
+function Character(props) {
+  return (
+    <Container>
+      <Row>
+        {props.characterArray.map((character) => {
+          return (
+            <Col xs="4">
+              <Card key={character.created}>
+                <BackgroundDiv>
+                  <CardTitle>{character.name}</CardTitle>
+
+                  <CardSubtitle>Gender: {character.gender}</CardSubtitle>
+                  <CardBody>
+                    <ContentTxt>
+                      Born: {character.birth_year}
+                      <br />
+                      Height: {character.height}
+                      <br />
+                      Mass: {character.mass}
+                    </ContentTxt>
+                  </CardBody>
+                </BackgroundDiv>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 }
 
-export default Character
+export default Character;
